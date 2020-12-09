468ac0aefe54f1b2d78d5ef92102ec4e1b3cc3a6
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-09 14:02:51 -0800

    modularize by separating scripts styles and htmls

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
